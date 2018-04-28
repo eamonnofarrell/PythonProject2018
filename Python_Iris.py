67f7857b07213cc48552ef10dc2fd5dff8a7ae60
eamonnofarrell <36230742+eamonnofarrell@users.noreply.github.com>
2018-04-28 14:23:40 +0100

    Add files via upload

--- Python_Iris.py
+++ Python_Iris.py
@@ -1,42 +1,48 @@
-#Eamonn O'Farrell
+#Eamonn O'Farrell - GMIT Python Project April 2018
 import numpy
 import pandas
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 
+
 # Load dataset
 url = "iris.csv"
+# assign column names
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-dataset = pandas.read_csv(url, names=names)
+IrisData = pandas.read_csv(url, names=names)
 
-# shape
-print(dataset.shape)
+# shape specifies the amount of rows and columns
+print(IrisData.shape)
 
-# head
-print(dataset.head(20))
+# head prints out data in IrisData
+print(IrisData.head(150))
 
-# descriptions
-print(dataset.describe())
+# pandas describe() calculates min, max, mean, etc 
+print(IrisData.describe())
 
-# class distribution
-print(dataset.groupby('class').size())
+# print amount of instances per class (flower type)
+print(IrisData.groupby('class').size())
 
-# Boxplot1 Use Pandas .boxplot() to calculate and show box and whisker plots
-dataset.boxplot()
+# Boxplot1 Use Pandas .boxplot() to calculate and show generic box and whisker plot
+IrisData.boxplot()
 plt.show()
 
-# Boxplot2 - use .boxset() to generate box plot howing varying 
+# Boxplot2 - use .boxset() to generate box plot showing varying 
 # sepal lengths per flower type
-dataset.boxplot(column="sepal-length",by="class")
- plt.show()
-
-
-# Use Pandas histograms .hist() to plot histogram of Iris Data Set
-dataset.hist()
+IrisData.boxplot(column="sepal-length",by="class")
 plt.show()
 
 
-# scatter plot matrix
-scatter_matrix(dataset)
+# Use Pandas histograms .hist() to plot standard histogram of Iris Data Set
+IrisData.hist()
 plt.show()
+
+# Use .hist() to filter sepal-length column and plot histogram 
+IrisData.hist(column='sepal-length')
+plt.show()
+
+# scatter plot matrix with data from csv file
+scatter_matrix(IrisData)
+plt.show()
+
 
